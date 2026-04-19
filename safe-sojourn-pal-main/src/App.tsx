03569add7ea924c41734338e6de507e3f0a6710c
GudimallaSujana <sujjugud@gmail.com>
2026-04-19 22:10:08 +0530

    changed icons

--- safe-sojourn-pal-main/src/App.tsx
+++ safe-sojourn-pal-main/src/App.tsx
@@ -1,4 +1,4 @@
-// import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -17,7 +17,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      {/* <Toaster /> */}
+      <Toaster />
       <Sonner />
       <BrowserRouter>
         <Navigation />
@@ -28,7 +28,7 @@ const App = () => (
           <Route path="/info" element={<TouristInfo />} />
           <Route path="/security" element={<SecurityVerification />} />
           <Route path="/admin" element={<Admin />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
